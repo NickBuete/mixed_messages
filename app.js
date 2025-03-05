53206a19985db6b2751604c14950d5b9d864a8f5
Nicholas Buete <nbuete01@gmail.com>
2025-03-05 21:36:47 +1100

    Added TODO list in app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// first file
+// Mixed messages project
+
+// TODO: Create a random message when the program runs
+
+
+// TODO: Creat an array of messages
+
+// TODO: Create a function that generates a random message
+
