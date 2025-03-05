888c1b407ddcd225049ba6b3f3235a698db8bbc5
Nicholas Buete <nbuete01@gmail.com>
2025-03-05 21:46:24 +1100

    added code to app.js with the messages array

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // Mixed messages project
-
+console.log('This is the Mixed Messages project');
 // TODO: Create a random message when the program runs
 
-
 // TODO: Creat an array of messages
-
+const messages = [
+    'Hello, World!',
+    'This is a random message',
+    'I am learning JavaScript',
+    'I am learning to code',
+    'I am learning to code in JavaScript',
+    'I am learning to code in JavaScript using Node.js',
+    'I am learning to code in JavaScript using Node.js and Git',
+    'I am learning to code in JavaScript using Node.js and Git and GitHub',
+    'I am learning to code in JavaScript using Node.js and Git and GitHub and VSCode',
+    'I am learning to code in JavaScript using Node.js and Git and GitHub and VSCode and the Command Line',
+];
 // TODO: Create a function that generates a random message
 
